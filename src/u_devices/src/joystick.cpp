620e2e3392dc1080b38d85523d21b3f1b10c88c4
isnow4ever <gaoyongzhuo@yeah.net>
2018-02-05 11:06:21 +0800

    marsboard init

--- src/u_devices/src/joystick.cpp
+++ src/u_devices/src/joystick.cpp
@@ -125,8 +125,7 @@ int main(int argc, char *argv[])
 
     ros::NodeHandle n;
     ros::Publisher joystick_pub = n.advertise<std_msgs::String>("u_pendant/devices/joystick", 1000);
-    ros::Subscriber joystick_sub
-            = n.subscribe("u_pendant/devices/control", 2, callback);
+    ros::Subscriber joystick_sub = n.subscribe("u_pendant/devices/control", 2, callback);
 
     ros::Rate loop_rate(50);
 
